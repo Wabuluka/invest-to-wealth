f98349d8a3aee19e5d5a8815fd0c8e1c4e5cfd6e
davieswabuluka <dwabuluka@gmail.com>
2017-08-29 06:57:27 +0300

    update
    i have added a bootstrap file as well as setting up the sass file to work directly. install koala or any related program to help you process sass into css as well.

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<link href = "bootstrap/css/bootstrap.css" rel = "stylesheet">
+		<link href = "bootstrap/css/custom.css" rel = "stylesheet">
 	</head>
-<body></body>
+<body>
+	<h1>
+		heading
+	</h1>
+</body>
 </html>
